59d46bf556aedd18361d2b1bfce3712ef7a2cb5c
yobange <recum12233@mail.ru>
2025-06-18 21:00:08 +0300

    more tests

--- src/main/java/com/pipemasters/server/repository/TrainRepository.java
+++ src/main/java/com/pipemasters/server/repository/TrainRepository.java
@@ -3,5 +3,5 @@ package com.pipemasters.server.repository;
 import com.pipemasters.server.entity.Train;
 
 public interface TrainRepository extends GeneralRepository<Train, Long> {
-
+    void deleteById(Long id);
 }
