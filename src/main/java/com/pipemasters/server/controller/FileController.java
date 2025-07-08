fbe6976dc9c28f9921cb292abca1a52dd51e6374
wmanzr <eablofen@list.ru>
2025-07-08 21:57:19 +0300

    edit controller

--- src/main/java/com/pipemasters/server/controller/FileController.java
+++ src/main/java/com/pipemasters/server/controller/FileController.java
@@ -22,7 +22,7 @@ public class FileController {
     }
 
     @PostMapping("/upload-url-audio")
-    public ResponseEntity<String> getPresignedUploadUrlForAudio(@RequestBody String sourceKey) {
+    public ResponseEntity<String> getPresignedUploadUrlForAudio(@PathVariable String sourceKey) {
         String url = fileService.generatePresignedUploadUrlForAudio(sourceKey);
         return ResponseEntity.ok(url);
     }
