96bbb68fc1c01c93e5a1c02c866d1cc92423d049
yobange <recum12233@mail.ru>
2025-07-16 15:24:54 +0300

    added new domain for cors

--- src/main/java/com/pipemasters/server/config/WebConfig.java
+++ src/main/java/com/pipemasters/server/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:*","https://*.obamus.ru", "http://92.242.60.137:*")
+                .allowedOriginPatterns("http://localhost:*","https://*.obamus.ru", "http://92.242.60.137:*", "https://frontendd-bice.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
