8c0d4ae5057f15ff8fe475b54c746cacdaf175c4
yobange <recum12233@mail.ru>
2025-07-21 02:39:25 +0300

    added CacheController with single method to clear cache.

---
+++ src/main/java/com/pipemasters/server/controller/CacheController.java
@@ -0,0 +1,28 @@
+package com.pipemasters.server.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cache")
+public class CacheController {
+
+    private final CacheManager cacheManager;
+
+    @Autowired
+    public CacheController(CacheManager cacheManager) {
+        this.cacheManager = cacheManager;
+    }
+
+    @DeleteMapping("/flush")
+    public void flushAllCaches() {
+        cacheManager.getCacheNames().forEach(name -> {
+            if (cacheManager.getCache(name) != null) {
+                cacheManager.getCache(name).clear();
+            }
+        });
+    }
+}
