372cb3e8da4b357bc4c7ea40106277d0f24985b6
yobange <recum12233@mail.ru>
2025-07-25 01:12:28 +0300

    удалил каку

--- src/main/java/com/pipemasters/server/repository/specifications/UploadBatchSpecifications.java
+++ src/main/java/com/pipemasters/server/repository/specifications/UploadBatchSpecifications.java
@@ -69,34 +69,6 @@ public class UploadBatchSpecifications {
                 p.add(cb.equal(br.get("id"), f.getBranchId()));
             }
 
-//            if (f.getTagIds() != null && !f.getTagIds().isEmpty()) {
-//                assert query != null;
-//                // Возвращает только те UploadBatch, которые содержат все теги из f.getTagIds()
-////                Join<UploadBatch, MediaFile> mediaFileJoin = root.join("files");
-////                Join<MediaFile, TagInstance> tagInstanceJoin = mediaFileJoin.join("tagInstances");
-////                Join<TagInstance, TagDefinition> tagDefinitionJoin = tagInstanceJoin.join("definition");
-////
-////                p.add(tagDefinitionJoin.get("id").in(f.getTagIds()));
-////
-////                query.groupBy(root.get("id"));
-////                query.having(cb.equal(cb.countDistinct(tagDefinitionJoin.get("id")), f.getTagIds().size()));
-////                query.distinct(true);
-//
-//                // Возвращает UploadBatch, которые содержат хотя бы один из тегов из f.getTagIds()
-//                for (Long tagId : f.getTagIds()) {
-//                    Subquery<Long> subquery = query.subquery(Long.class);
-//                    Root<UploadBatch> subRoot = subquery.from(UploadBatch.class);
-//                    Join<UploadBatch, MediaFile> fileJoin = subRoot.join("files");
-//                    Join<MediaFile, TagInstance> tagJoin = fileJoin.join("tagInstances");
-//                    Join<TagInstance, TagDefinition> defJoin = tagJoin.join("definition");
-//
-//                    subquery.select(subRoot.get("id"))
-//                            .where(cb.equal(subRoot.get("id"), root.get("id")),
-//                                    cb.equal(defJoin.get("id"), tagId));
-//                    p.add(cb.exists(subquery));
-//                }
-//            }
-
             if (f.getTagIds() != null && !f.getTagIds().isEmpty()) {
                 Subquery<Long> sub = query.subquery(Long.class);
                 Root<UploadBatch> subBatch = sub.from(UploadBatch.class);
